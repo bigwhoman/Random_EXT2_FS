e639c7ad563821a0392ab1f8836447bd87583b5a
bigwhoman <hm.keshvari@gmail.com>
2024-10-13 16:16:52 -0400

    adding a filesystem explorer module

--- src/fs-explorer.c
+++ src/fs-explorer.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdint.h>
+#include "ext2-headers.h"
 
 #define BASE_OFFSET 1024                   /* locates beginning of the super block (first group) */
 #define FD_DEVICE "./hello.img"              /* the floppy disk device */
@@ -12,49 +13,10 @@
 
 static unsigned int block_size = 0;        /* block size (to be calculated) */
 
-struct ext2_super_block {
-    uint32_t s_inodes_count;
-    uint32_t s_blocks_count;
-    uint32_t s_r_blocks_count;
-    uint32_t s_free_blocks_count;
-    uint32_t s_free_inodes_count;
-    uint32_t s_first_data_block;
-    uint32_t s_log_block_size;
-    uint32_t s_log_frag_size;
-    uint32_t s_blocks_per_group;
-    uint32_t s_frags_per_group;
-    uint32_t s_inodes_per_group;
-    uint32_t s_mtime;
-    uint32_t s_wtime;
-    uint16_t s_mnt_count;
-    uint16_t s_max_mnt_count;
-    uint16_t s_magic;
-    uint16_t s_state;
-    uint16_t s_errors;
-    uint16_t s_minor_rev_level;
-    uint32_t s_lastcheck;
-    uint32_t s_checkinterval;
-    uint32_t s_creator_os;
-    uint32_t s_rev_level;
-    uint16_t s_def_resuid;
-    uint16_t s_def_resgid;
-    uint32_t s_first_ino;
-    uint16_t s_inode_size;
-    uint16_t s_block_group_nr;
-    uint32_t s_feature_compat;
-    uint32_t s_feature_incompat;
-    uint32_t s_feature_ro_compat;
-    uint8_t  s_uuid[16];
-    char     s_volume_name[16];
-    char     s_last_mounted[64];
-    uint32_t s_algorithm_usage_bitmap;
-    // ... (rest of the structure omitted for brevity)
-};
-
 
 int main(void)
 {
-	struct ext2_super_block super;
+	struct ext2_superblock super;
 	int fd;
 
 	/* open floppy device */
@@ -100,9 +62,7 @@ int main(void)
 	       block_size,
 	       super.s_blocks_per_group,
 	       super.s_inodes_per_group,
-	       super.s_creator_os,
-	       super.s_first_ino,          /* first non-reserved inode */
-	       super.s_inode_size);
+	       super.s_creator_os);
 	
 	exit(0);
 } /* main() */

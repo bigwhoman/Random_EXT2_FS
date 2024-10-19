2ce965b0ef1d24de749094508f37b846fa0bb23a
bigwhoman <hm.keshvari@gmail.com>
2024-10-18 21:27:21 -0400

    completed bitmaps, inode tables and directory read

--- src/fs-explorer.c
+++ src/fs-explorer.c
@@ -79,7 +79,7 @@ int main(void)
 	struct ext2_block_group_descriptor group_descriptor_table[groups + 1];
 	for (size_t i = 0; i < groups; i++)
 	{
-		printf("group : %d\n", i);
+		// printf("group : %d\n", i);
 		lseek(fd, block_size * (i + 1), SEEK_SET);
 		read(fd, &group_descriptor_table[i], sizeof(group_descriptor_table[i]));
 
@@ -98,6 +98,97 @@ int main(void)
 			group_descriptor_table[i].bg_free_blocks_count,
 			group_descriptor_table[i].bg_free_inodes_count,
 			group_descriptor_table[i].bg_used_dirs_count);
+	}
+
+	int *block_bitmap = malloc(sizeof(int));
+	lseek(fd, FIND_BLOCK_OFFSET(group_descriptor_table[0].bg_block_bitmap), SEEK_SET);
+
+	printf("find %d\n", super.s_blocks_count / (32));
+	for (u32 i = 0; i < super.s_blocks_count / (sizeof(int) * 8); i++)
+	{
+
+		read(fd, block_bitmap, sizeof(int));
+		for (char j = 0; j < 32; j++)
+		{
+			if ((*block_bitmap) & (1 << j))
+				printf("block present : %u\n", i * 32 + j);
+		}
+	}
+
+	int *inode_bitmap = malloc(sizeof(int));
+	lseek(fd, FIND_BLOCK_OFFSET(group_descriptor_table[0].bg_inode_bitmap), SEEK_SET);
+
+	// printf("find %d\n", super.s_inodes_per_group/(32));
+	for (u32 i = 0; i < super.s_inodes_per_group / (sizeof(int) * 8); i++)
+	{
+
+		read(fd, inode_bitmap, sizeof(int));
+		for (char j = 0; j < 32; j++)
+		{
+			// Inefficient AF
+			if ((*inode_bitmap) & (1 << j)) /* Inodes start at 1 lol :D */
+				printf("inode present : %u\n", i * 32 + j + 1);
+		}
+	}
+
+	struct ext2_inode inode_table[super.s_inodes_per_group];
+
+	lseek(fd, FIND_BLOCK_OFFSET((group_descriptor_table[0].bg_inode_table)), SEEK_SET);
+	for (size_t i = 0; i < super.s_first_ino + 1; i++)
+	{
+		read(fd, &inode_table[i], sizeof(inode_table[i]));
+	}
+
+	/* print root inode thingies*/
+	printf(
+		"imode          		: %x\n"
+		"uid           : %u\n"
+		"size            : %hu\n"
+		"gid            : %u\n"
+		"link count           : %u\n"
+		"blocks             : %hu\n"
+		"flags             : %hu\n"
+		"address             : %hu\n",
+		inode_table[EXT2_ROOT_INO].i_mode,
+		inode_table[EXT2_ROOT_INO].i_uid,
+		inode_table[EXT2_ROOT_INO].i_size,
+		inode_table[EXT2_ROOT_INO].i_gid,
+		inode_table[EXT2_ROOT_INO].i_links_count,
+		inode_table[EXT2_ROOT_INO].i_blocks,
+		inode_table[EXT2_ROOT_INO].i_flags,
+		inode_table[EXT2_ROOT_INO].i_faddr);
+	struct ext2_dir_entry root[20];
+	for (size_t i = 0; i < 16; i++)
+	{
+		printf("block ---> %d\n", inode_table[EXT2_ROOT_INO].i_block[i]);
+		if (inode_table[EXT2_ROOT_INO].i_block[i] != 0)
+		{
+			if (inode_table[EXT2_ROOT_INO].i_mode & EXT2_S_IFDIR)
+			{
+				lseek(fd, FIND_BLOCK_OFFSET(inode_table[EXT2_ROOT_INO].i_block[i]), SEEK_SET);
+				int j = 0;
+				do
+				{
+					read(fd, &root[j], 8);
+					
+					read(fd, &root[j].name, root[j].rec_len - 8);
+					j++;
+				} while (root[j - 1].rec_len > 0 && (lseek(fd, 0, SEEK_CUR) + root[j - 1].rec_len < FIND_BLOCK_OFFSET(inode_table[EXT2_ROOT_INO].i_block[i]) + block_size));
+			}
+		}
+	}
+
+	int i = 0;
+	while (1)
+	{
+		printf(
+			"inode          		: %d\n"
+			"name          		: %s\n",
+			root[i].inode,
+			root[i].name);
+		if (root[i].rec_len <= 0)
+			break;
+		i++;
 	}
 
 	close(fd);

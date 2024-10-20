8bfc860ec022fa5424df25fadb68ba240689a4b6
bigwhoman <hm.keshvari@gmail.com>
2024-10-19 23:48:00 -0400

    completed traversal

--- src/fs-explorer.c
+++ src/fs-explorer.c
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include "ext2-headers.h"
 #include <math.h>
+#include <string.h>
 /* locates beginning of the super block (first group) */
 #define FD_DEVICE "ext2_filesystem_reference.img" /* the floppy disk device */
 #define EXT2_SUPER_MAGIC 0xEF53
@@ -136,7 +137,7 @@ int main(void)
 	lseek(fd, FIND_BLOCK_OFFSET((group_descriptor_table[0].bg_inode_table)), SEEK_SET);
 	for (size_t i = 0; i < super.s_first_ino + 1; i++)
 	{
-		read(fd, &inode_table[i], sizeof(inode_table[i]));
+		read(fd, &inode_table[i], super.s_inode_size);
 	}
 
 	/* print root inode thingies*/
@@ -149,46 +150,151 @@ int main(void)
 		"blocks             : %hu\n"
 		"flags             : %hu\n"
 		"address             : %hu\n",
-		inode_table[EXT2_ROOT_INO].i_mode,
-		inode_table[EXT2_ROOT_INO].i_uid,
-		inode_table[EXT2_ROOT_INO].i_size,
-		inode_table[EXT2_ROOT_INO].i_gid,
-		inode_table[EXT2_ROOT_INO].i_links_count,
-		inode_table[EXT2_ROOT_INO].i_blocks,
-		inode_table[EXT2_ROOT_INO].i_flags,
-		inode_table[EXT2_ROOT_INO].i_faddr);
+		inode_table[EXT2_ROOT_INO - 1].i_mode,
+		inode_table[EXT2_ROOT_INO - 1].i_uid,
+		inode_table[EXT2_ROOT_INO - 1].i_size,
+		inode_table[EXT2_ROOT_INO - 1].i_gid,
+		inode_table[EXT2_ROOT_INO - 1].i_links_count,
+		inode_table[EXT2_ROOT_INO - 1].i_blocks,
+		inode_table[EXT2_ROOT_INO - 1].i_flags,
+		inode_table[EXT2_ROOT_INO - 1].i_faddr);
 	struct ext2_dir_entry root[20];
-	for (size_t i = 0; i < 16; i++)
+	for (size_t i = 0; i < 15; i++)
 	{
-		printf("block ---> %d\n", inode_table[EXT2_ROOT_INO].i_block[i]);
-		if (inode_table[EXT2_ROOT_INO].i_block[i] != 0)
+		printf("block ---> %d\n", inode_table[EXT2_ROOT_INO - 1].i_block[i]);
+		if (inode_table[EXT2_ROOT_INO - 1].i_block[i] != 0)
 		{
-			if (inode_table[EXT2_ROOT_INO].i_mode & EXT2_S_IFDIR)
+			if (inode_table[EXT2_ROOT_INO - 1].i_mode & EXT2_S_IFDIR)
 			{
-				lseek(fd, FIND_BLOCK_OFFSET(inode_table[EXT2_ROOT_INO].i_block[i]), SEEK_SET);
+				lseek(fd, FIND_BLOCK_OFFSET(inode_table[EXT2_ROOT_INO - 1].i_block[i]), SEEK_SET);
 				int j = 0;
 				do
 				{
 					read(fd, &root[j], 8);
-					
+
 					read(fd, &root[j].name, root[j].rec_len - 8);
 					j++;
-				} while (root[j - 1].rec_len > 0 && (lseek(fd, 0, SEEK_CUR) + root[j - 1].rec_len < FIND_BLOCK_OFFSET(inode_table[EXT2_ROOT_INO].i_block[i]) + block_size));
+				} while (root[j - 1].rec_len > 0 && (lseek(fd, 0, SEEK_CUR) + root[j - 1].rec_len < FIND_BLOCK_OFFSET(inode_table[EXT2_ROOT_INO - 1].i_block[i]) + block_size));
 			}
 		}
 	}
 
-	int i = 0;
-	while (1)
+	// int i = 0;
+	// while (1)
+	// {
+	// 	printf(
+	// 		"inode          		: %d\n"
+	// 		"name          		: %s\n",
+	// 		root[i].inode,
+	// 		root[i].name);
+	// 	if (root[i].rec_len <= 0)
+	// 		break;
+	// 	i++;
+	// }
+
+	char *get_dir[] = {"hoolp", "hi", "hello.txt"};
+	int len = sizeof(get_dir) / sizeof(char *);
+	int father_node = 2;
+	int inode_size = super.s_inode_size;
+	for (size_t i = 0; i < len; i++)
 	{
-		printf(
-			"inode          		: %d\n"
-			"name          		: %s\n",
-			root[i].inode,
-			root[i].name);
-		if (root[i].rec_len <= 0)
+		int wrong_input = 0;
+		// printf("inode table at : %x -\n",FIND_BLOCK_OFFSET(group_descriptor_table[0].bg_inode_table) + (father_node - 1) * inode_size);
+		lseek(fd, FIND_BLOCK_OFFSET(group_descriptor_table[0].bg_inode_table) + (father_node - 1) * inode_size, SEEK_SET);
+		struct ext2_inode this_inode;
+		read(fd, &this_inode, inode_size);
+		// printf("super size : %d\n",super.s_inode_size);
+		printf("\n\n"
+			   "imode          		: %x\n"
+			   "uid           : %u\n"
+			   "size            : %hu\n"
+			   "gid            : %u\n"
+			   "link count           : %u\n"
+			   "blocks             : %hu\n"
+			   "flags             : %hu\n"
+			   "block 0             : %hu\n"
+			   "block 1             : %hu\n"
+			   "address             : %hu\n",
+			   this_inode.i_mode,
+			   this_inode.i_uid,
+			   this_inode.i_size,
+			   this_inode.i_gid,
+			   this_inode.i_links_count,
+			   this_inode.i_blocks,
+			   this_inode.i_flags,
+			   this_inode.i_block[0],
+			   this_inode.i_block[1],
+			   this_inode.i_faddr);
+		// printf("superrrrrrrrrrrrrr size : %d\n",super.s_inode_size);
+
+		if (this_inode.i_mode & EXT2_S_IFDIR)
+		{
+
+			for (size_t block = 0; block < 15; block++)
+			{
+
+				int found = 0;
+				if (this_inode.i_block[block] != 0)
+				{
+					// printf("inode block : %d\n", this_inode.i_block[block]);
+					lseek(fd, FIND_BLOCK_OFFSET(this_inode.i_block[block]), SEEK_SET);
+					int j = 0;
+					struct ext2_dir_entry entry;
+					int sss = 0;
+					do
+					{
+						read(fd, &entry, 8);
+
+						read(fd, &entry.name, entry.rec_len - 8);
+						// printf("supaaaa size : %d\n",super.s_inode_size);
+						if (entry.inode < 1)
+							break;
+						// printf(" yoooooo ------> %s ===== %s --- %d\n", entry.name, get_dir[i], lseek(fd, 0, SEEK_CUR));
+						if (!strncmp(entry.name, get_dir[i], sizeof(get_dir[i])))
+						{
+
+							wrong_input = 0;
+							father_node = entry.inode;
+							found = 1;
+							break;
+						}
+						sss++;
+					} while (1);
+					if (!found)
+						wrong_input = 1;
+				}
+				// if (wrong_input)
+				// 	break;
+				if (found)
+				{
+					// printf("new node : %d\n", father_node);
+					break;
+				}
+			}
+		}
+		else
+		{
+			for (size_t block = 0; block < 15; block++)
+			{
+
+				int found = 0;
+				if (this_inode.i_block[block] != 0)
+				{
+					// printf("inode block : %d\n", this_inode.i_block[block]);
+					char buffer[block_size];
+					lseek(fd, FIND_BLOCK_OFFSET(this_inode.i_block[block]), SEEK_SET);
+					ssize_t bytes_read = read(fd, buffer, block_size);
+					printf("file content ---------------> \n "
+							"%s \n"
+					,buffer);
+				}
+			}
+		}
+		if (wrong_input)
+		{
+			printf("wrong input shit\n");
 			break;
-		i++;
+		}
 	}
 
 	close(fd);
